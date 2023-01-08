99a597cceb55c440ea954cac5482890331082f25
galleonpt <jose15.luis00@gmail.com>
2023-01-08 20:30:45 +0000

    rename outdir build folder

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/bessa-portfolio/docs",
+  base: "/",
   plugins: [react()],
-  build: { outDir: "docs" },
 });
