258afc0d2d0942683b7c599791459d1061cc63c1
galleonpt <jose15.luis00@gmail.com>
2023-01-08 20:12:56 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/docs",
+  base: "/bessa-portfolio/dist",
   plugins: [react()],
-  build: { outDir: "docs" },
+  // build: { outDir: "docs" },
 });
